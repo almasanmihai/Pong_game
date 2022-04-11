615fe354319ecb453eed273e1989b0a48ed282c7
almasanmihai <almasanmihai@yahoo.com>
2022-04-11 18:27:48 +0300

    Changed the collision with pads.

--- main.py
+++ main.py
@@ -27,23 +27,29 @@ while is_on:
     screen.update()
     time.sleep(ball.move_speed)
     ball.move()
+    ball_position = [int(ball.xcor()), int(ball.ycor())]
+    l_pad_position = []
+    r_pad_position = []
+    for i in range(9):
+        lpos = [l_pad.xcor() + 20, l_pad.ycor() - 40 + 10 * i]
+        l_pad_position.append(lpos)
+        rpos = [r_pad.xcor() -20, r_pad.ycor() - 40 + 10 * i]
+        r_pad_position.append(rpos)
+
+    if ball_position in l_pad_position or ball_position in r_pad_position:
+        ball.bounce_pad()
 
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_side()
 
-    if ball.xcor() > 350:
+    if ball.xcor() > 360:
         ball.reset_pos()
         score.l_score_up()
 
-    if ball.xcor() < -350:
+    if ball.xcor() < -360:
         ball.reset_pos()
         score.r_score_up()
 
-    if ball.distance(r_pad) < 50 and ball.xcor() > 330:
-        ball.bounce_pad()
-
-    if ball.distance(l_pad) < 50 and ball.xcor() < -330:
-        ball.bounce_pad()
     if score.game_over():
         is_on = False
 
